ca7ddd11b9141f2ce0140431eb7c81a49976f781
Szymon Katra <szymon.katra@gmail.com>
2018-03-06 01:03:46 +0100

    Using canvas for chart

--- src/audioanalyzer/Main.java
+++ src/audioanalyzer/Main.java
@@ -3,12 +3,17 @@ package audioanalyzer;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.chart.AreaChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
@@ -17,7 +22,7 @@ import java.io.*;
 public class Main extends Application {
 
     private static double[] m_fftResult;
-    private static final int SAMPLESCOUNT = 8192;
+    private static final int SAMPLESCOUNT = 512;
 
     public static void main(String[] args) {
         double[] samples = new double[SAMPLESCOUNT];
@@ -53,7 +58,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Hello World!");
 
-        NumberAxis xAxis = new NumberAxis(0, 22050,10);
+        /*NumberAxis xAxis = new NumberAxis(0, 22050,10);
         NumberAxis yAxis = new NumberAxis(0, 0.1, 0.0001);
         AreaChart<Number, Number> chart = new AreaChart<Number, Number>(xAxis, yAxis);
 
@@ -72,7 +77,30 @@ public class Main extends Application {
 
         Scene scene = new Scene(chart, 800, 600);
         chart.getData().addAll(series);
+        primaryStage.setScene(scene);*/
+
+        Canvas canvas = new Canvas(800, 600);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gc.setFill(Color.RED);
+        double prevX = 0, prevY = 0;
+        for (int i = 0; i < m_fftResult.length; i++)
+        {
+            double x = (i * (44100.0 / SAMPLESCOUNT)) / 22050 * 800;
+            double y = 600 - (m_fftResult[i] * 600);
+            gc.strokeLine(prevX, prevY, x, y);
+
+            prevX = x;
+            prevY = y;
+        }
+
+        Pane root = new Pane();
+        root.getChildren().add(canvas);
+        Scene scene = new Scene(root);
+
+
         primaryStage.setScene(scene);
+        primaryStage.setTitle("FFT");
         primaryStage.show();
 
     }
