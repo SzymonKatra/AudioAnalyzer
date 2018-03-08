c5afbd21d66b3e970671832bc40226b9d037f884
Szymon Katra <szymon.katra@gmail.com>
2018-03-08 23:17:29 +0100

    Cleanup FFTAnalyzer

--- src/audioanalyzer/logic/FFTAnalyzer.java
+++ src/audioanalyzer/logic/FFTAnalyzer.java
@@ -68,40 +68,4 @@ public class FFTAnalyzer implements ISignalAnalyzer {
             samples[index + i + halfN] = even.subtract(factor.multiply(odd));
         }
     }
-
-    /*public static Complex[] fft(Complex[] x) {
-        int n = x.length;
-
-        // base case
-        if (n == 1) return new Complex[] { x[0] };
-
-        // radix 2 Cooley-Tukey FFT
-        if (n % 2 != 0) {
-            throw new IllegalArgumentException("n is not a power of 2");
-        }
-
-        // fft of even terms
-        Complex[] even = new Complex[n/2];
-        for (int k = 0; k < n/2; k++) {
-            even[k] = x[2*k];
-        }
-        Complex[] q = fft(even);
-
-        // fft of odd terms
-        Complex[] odd  = even;  // reuse the array
-        for (int k = 0; k < n/2; k++) {
-            odd[k] = x[2*k + 1];
-        }
-        Complex[] r = fft(odd);
-
-        // combine
-        Complex[] y = new Complex[n];
-        for (int k = 0; k < n/2; k++) {
-            double kth = -2 * k * Math.PI / n;
-            Complex wk = new Complex(Math.cos(kth), Math.sin(kth));
-            y[k]       = q[k].add(wk.multiply(r[k]));
-            y[k + n/2] = q[k].subtract(wk.multiply(r[k]));
-        }
-        return y;
-    }*/
 }
